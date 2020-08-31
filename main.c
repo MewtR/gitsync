363d240cd46bd42468c017f76790a186a8a30747
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2020-08-31 06:43:22 -0400

    Print each argument passed at the command line

--- main.c
+++ main.c
@@ -4,5 +4,15 @@ int main(int argc, char * argv[])
 {
     printf("Here is the number of arguments I received: %i \n", argc);
 
+    int i = 0;
+    for(char** av = argv; *av; av++){ //argv ends with null pointer
+        if(i == 0){
+            printf("The program name is: %s \n", argv[0]);
+        }else{
+            printf("Argument number %d is: %s \n", i, argv[i]);
+        }
+        i++;
+    }
+
     return 0;
 }
