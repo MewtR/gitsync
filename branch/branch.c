3e0529a05fbd2d78c6bbdd2d58d878d2a6e6dad1
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2020-09-04 20:58:34 -0400

    Only print error from libgit2 if there is one.

--- branch/branch.c
+++ branch/branch.c
@@ -1,5 +1,19 @@
 #include "branch.h"
+#include <git2/buffer.h>
 
+int get_remote_name(git_repository * repo, git_buf* remote_branch)
+{
+    git_buf buf = {NULL,0,0};
+    int status = git_branch_remote_name(&buf, repo, remote_branch->ptr);
+    if(status){
+        git_buf_dispose(&buf);
+        return handle_error("Error getting the remote name of %s", remote_branch->ptr, status);
+    }else{
+        printf("The remote of %s is %s \n", remote_branch->ptr, buf.ptr);
+    }
+    git_buf_dispose(&buf);
+    return 0;
+}
 int get_current_upstream_branch(git_repository *repo,git_reference* ref)
 {
     const char* ref_name = git_reference_name(ref);
@@ -11,8 +25,9 @@ int get_current_upstream_branch(git_repository *repo,git_reference* ref)
         return handle_error("Error getting upstream name of branch %s \n", ref_name, status);
     } else {
         printf("Upstream branch name of %s is %s \n", ref_name, buf.ptr);
-        git_buf_dispose(&buf);
+        get_remote_name(repo, &buf);
     }
+    git_buf_dispose(&buf);
     return 0;
 }
 
--- error/error.c
+++ error/error.c
@@ -6,9 +6,9 @@ char* get_error_message(const char* format,const char* argument)
   
   size_t needed = 0;
   if(argument)
-      needed = snprintf(NULL, 0, format, argument) + 1;
+      needed = snprintf(NULL, 0, format, argument) + 1; // + 1 to account for null terminator
   else
-      needed = strlen(format);
+      needed = strlen(format); // again the null terminator
   char *buffer = malloc(needed);
   sprintf(buffer, format, argument);
   return buffer;
@@ -19,7 +19,7 @@ void log_error(char* message, int status)
     printf("%s", message);
     //Error message from libgit2
     const git_error *e = git_error_last();
-    printf("Error %d/%d: %s\n", status, e->klass, e->message);
+    if(e) printf("Error %d/%d: %s\n", status, e->klass, e->message);
     free(message);
 }
 
